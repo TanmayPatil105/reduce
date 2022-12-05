1e23a9e46e4a416fe92e86e216b764cbf3f35761
Tanmay Patil <tanmaynpatil105@gmail.com>
2022-12-06 01:14:45 +0530

    Added workflow

---
+++ go.mod
@@ -0,0 +1,18 @@
+module github.com/TanmayPatil105/reduced.to
+
+go 1.19
+
+require (
+	github.com/Delta456/box-cli-maker/v2 v2.3.0 // indirect
+	github.com/atotto/clipboard v0.1.4 // indirect
+	github.com/gookit/color v1.5.2 // indirect
+	github.com/huandu/xstrings v1.3.2 // indirect
+	github.com/mattn/go-runewidth v0.0.14 // indirect
+	github.com/muesli/reflow v0.3.0 // indirect
+	github.com/pborman/getopt/v2 v2.0.0-20200816005738-fd0d075bf4de // indirect
+	github.com/pborman/options v1.3.0 // indirect
+	github.com/rivo/uniseg v0.2.0 // indirect
+	github.com/xo/terminfo v0.0.0-20220910002029-abceb7e1c41e // indirect
+	golang.org/x/sys v0.1.0 // indirect
+	golang.org/x/term v0.0.0-20221017184919-83659145692c // indirect
+)
---
+++ main.go
@@ -0,0 +1,117 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+	"strings"
+	"github.com/Delta456/box-cli-maker/v2"
+	"github.com/atotto/clipboard"
+	// "github.com/pborman/options"
+)
+
+
+const (
+	NormalText       = "\033[0m" 
+	BlackText        = "\033[30m"
+	RedText          = "\033[31m"
+	GreenText        = "\033[32m"
+	YellowText       = "\033[33m"
+	BlueText         = "\033[34m"
+	MagentaText      = "\033[35m"
+	CyanText         = "\033[36m"
+	WhiteText        = "\033[37m"
+	DefaultColorText = "\033[39m" 
+	BoldText         = "\033[1m"
+	BoldBlackText    = "\033[1;30m"
+	BoldRedText      = "\033[1;31m"
+	BoldGreenText    = "\033[1;32m"
+	BoldYellowText   = "\033[1;33m"
+	BoldBlueText     = "\033[1;34m"
+	BoldMagentaText  = "\033[1;35m"
+	BoldCyanText     = "\033[1;36m"
+	FaintText        = "\033[2m"
+	FaintBlackText   = "\033[2;30m"
+	FaintRedText     = "\033[2;31m"
+	FaintGreenText   = "\033[2;32m"
+	FaintYellowText  = "\033[2;33m"
+	FaintBlueText    = "\033[2;34m"
+	FaintMagentaText = "\033[2;35m"
+	FaintCyanText    = "\033[2;36m"
+	FaintWhiteText   = "\033[2;37m"
+	DefaultText      = "\033[22;39m" 
+)
+
+func main(){
+	printAscii()
+
+	var inputUrl string
+	if(len(os.Args)==1){
+		fmt.Printf("\nEnter the URL : " + GreenText) 
+		fmt.Scanln(&inputUrl)
+		fmt.Printf("\n") 
+	} else if(os.Args[1] == "--clip" || os.Args[1] == "-c" ){
+		inputUrl,_ = clipboard.ReadAll()
+	}
+	if(checkUrl(inputUrl)){
+		postReq(inputUrl)
+	} else{
+		Box(RedText+"Invalid URL"+NormalText)
+	}
+}
+
+func Box(url string){
+	Box := box.New(box.Config{Px: 5, Py: 2, Type: "Round",TitlePos:"Top", TitleColor:"Green",Color: "Cyan"})
+ 	Box.Print("URL",url)
+}
+
+
+func printAscii(){
+	fmt.Println(BoldGreenText)
+	fmt.Println("                    __                        __     ")
+	fmt.Println("   _____ ___   ____/ /__  __ _____ ___   ____/ /     ")
+	fmt.Println("  / ___// _ \\ / __  // / / // ___// _ \\ / __  /    ")
+	fmt.Println(" / /   /  __// /_/ // /_/ // /__ /  __// /_/ /     ")
+	fmt.Println("/_/    \\___/ \\__ _/ \\__ _/ \\___/ \\___/ \\__ _/  ")
+	fmt.Println(NormalText)
+}
+
+func checkUrl(url string)bool{
+    resp,err := http.Get(url)
+    if err != nil {
+        return false
+    }
+    return resp.StatusCode == 200
+}
+
+func postReq(inputUrl string){
+	const myUrl = "https://reduced.to/api/v1/shortener"
+
+	var request string = "{ \"originalUrl\" : \"" + inputUrl + "\" }";
+
+	requestBody := strings.NewReader(request)
+
+	response, err := http.Post(myUrl,"application/json",requestBody)
+
+	if err != nil{
+		fmt.Println("ERROR1")
+		// panic(err)
+		return
+	}
+	defer response.Body.Close()
+
+	content , _ :=ioutil.ReadAll(response.Body)
+
+	data := []byte(content)
+
+	var dat map[string]interface{}
+
+	if err := json.Unmarshal(data, &dat); err != nil {
+        panic(err)
+    }
+	url := "https://reduced.to/"  + dat["newUrl"].(string)
+	
+	Box(url)
+}
